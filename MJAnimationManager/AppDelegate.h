654b2ce16f37e4df87646e9f6b6da37726dc29d6
刘鹏 <392009255@qq.com>
2018-04-13 18:25:30 +0800

    Initial Commit

---
+++ MJAnimationManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJAnimationManager
+//
+//  Created by 刘鹏 on 2018/4/13.
+//  Copyright © 2018年 musjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJAnimationManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJAnimationManager
+//
+//  Created by 刘鹏 on 2018/4/13.
+//  Copyright © 2018年 musjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
